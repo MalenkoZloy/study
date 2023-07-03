ce01df4fe52f64ab93b9e052525ca35f0babf458
MalenkoZloy <korobkov1812@gmail.com>
2023-07-03 15:20:45 +0700

    Дописал проверку

--- Task_9.py
+++ Task_9.py
@@ -11,18 +11,22 @@
 """
 
 
-def image_info(**my_dict):
-    if 'image_id' not in my_dict:
-        raise TypeError("Ключа: image_id нет в словаре!")
+def image_info(**my_dict): 
+    if 'image_id' not in my_dict and 'image_title' not in my_dict:
+        raise TypeError("Ключа images_title и image_id нет в словаре")
     elif 'image_title' not in my_dict:
         raise TypeError("Ключа: image_title нет в словаре")
+    elif 'image_id' not in my_dict:
+        raise TypeError("Ключа: image_id нет в словаре!")
     return f"Image {my_dict['image_title']} has id {my_dict['image_id']}"
+    
+        
 
 
 try:
     my_info = {
-        'image_title': 'my cat',
-        'image_id': 5136
+        'image_titl': 'my cat',
+        'image_i': 5136
     }
     result = image_info(**my_info)
     print(result)
